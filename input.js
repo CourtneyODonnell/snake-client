6a4f030b1d320999f4bb73308a69685b278bcd42
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-06-16 22:07:43 -0300

    begin WASD movement code

--- input.js
+++ input.js
@@ -1,17 +1,23 @@
-
+// Stores the active TCP connection object.
+let connection;
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   stdin.on('data', (userInput) => {
-    handleUserInput(userInput);
-  })
+    handleUserInput(userInput, conn);
+  });
 
   return stdin;
 };
+
+/*Add logic to the input.js file to console.log 
+the movement command to your screen if one of these
+keys is pressed. */
 
 module.exports = setupInput;

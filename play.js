a5589676404f36f8b2a4570965992cd5b549cb62
CourtneyODonnell <courtney.odonnell.cormier@gmail.com>
2022-06-16 21:42:53 -0300

    stdin setup

--- play.js
+++ play.js
@@ -4,3 +4,18 @@ const net = require('net');
 
 console.log("Connecting ...");
 connect();
+
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (userInput) => {
+    handleUserInput(input);
+  })
+
+  return stdin;
+};
